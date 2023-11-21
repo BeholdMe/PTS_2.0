22f21a7057e812a3d238e0fa8c562b8283c63aa5
Om Patil <om@Oms-MacBook-Air.lan>
2023-11-20 19:40:23 -0600

    Login and Registration

--- LoginAndRegistration/app/src/main/java/com/example/loginandregistration/HomePageActivity.java
+++ LoginAndRegistration/app/src/main/java/com/example/loginandregistration/HomePageActivity.java
@@ -1,2 +1,33 @@
-package com.example.loginandregistration;public class HomepageActivity {
+package com.example.loginandregistration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    //private TextView homepage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        //homepage = findViewById(R.id.homepage);
+    }
 }
