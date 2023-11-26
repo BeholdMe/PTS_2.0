c52f2b414c633e6c64c8e28a6b4440df75e53eeb
Balaputradewa Ratuwina <be.ratuwina@gmail.com>
2023-11-26 17:34:58 -0600

    Added Payments, modified homepageActivity

--- PTS/app/src/main/java/com/example/pts/HomePage/HomePageActivity.java
+++ PTS/app/src/main/java/com/example/pts/HomePage/HomePageActivity.java
@@ -1,57 +1,62 @@
 package com.example.pts.HomePage;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 import com.example.pts.R;
+import android.widget.Button;
+import android.view.View;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private Button btnTutoringCategories, btnBecomeATutor, btnSearch;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+        //The three vars are the buttons from the layout of HomePage
         btnTutoringCategories = findViewById(R.id.btnTutoringCategories);
         btnBecomeATutor = findViewById(R.id.btnBecomeATutor);
-        btnSearch = findViewById(R.id.btnSearch);
+        btnSearch = findViewById(R.id.btnSearchHome);
 
-        btnTutoringCategories.setOnClickListener(new View.OnClickListener() {
+        //Listeners to send user to the page they click on
+        btnTutoringCategories.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 startActivity(new Intent(com.example.pts.HomePage.HomePageActivity.this, com.example.pts.TutoringCategories.ViewCategoryActivity.class));
             }
         });
 
-        btnBecomeATutor.setOnClickListener(new View.OnClickListener() {
+        btnBecomeATutor.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 startActivity(new Intent(com.example.pts.HomePage.HomePageActivity.this, com.example.pts.BecomeATutor.BecomeATutorActivity.class));
             }
         });
 
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        btnSearch.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 startActivity(new Intent(com.example.pts.HomePage.HomePageActivity.this, com.example.pts.Search.SearchActivity.class));
             }
         });
     }
 }
+
+/*Put in Android Manifest file in case other doesn't work
+<activity
+            android:name=".HomePage.HomePageActivity"
+            android:exported="false"
+            android:label="@string/title_activity_home_page"
+            android:theme="@style/Theme.PTS.NoActionBar" />
+ */
